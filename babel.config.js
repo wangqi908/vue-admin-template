29643c70934dda74411f12c93baa9869c411310b
wangqi908 <wangqi908@hotmail.com>
2019-11-07 13:24:48 +0800

    配置按需引入elementUi

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    "@vue/app",
+  ],
+  plugins: [
+    // element官方教程
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk"
+      }
+    ]
   ]
-}
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>ww</el-button>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@c/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      input: ""
+    };
   }
-}
+};
 </script>
