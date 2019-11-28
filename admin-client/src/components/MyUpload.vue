39913dd1be6b74eff7c9f92953ee8eca92b235e8
wangqi908 <wangqi908@hotmail.com>
2019-11-28 15:01:04 +0800

    增加注释

--- admin-client/src/components/MyUpload.vue
+++ admin-client/src/components/MyUpload.vue
@@ -121,7 +121,6 @@ export default {
   },
   computed: {
    ...mapState(["uploadProgress"]),
-
   },
   watch: {
      uploadProgress(val){
--- admin-client/src/store/index.js
+++ admin-client/src/store/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
+import createPersistedState from 'vuex-persistedstate' //vuex数据持久化
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    num: 0,
     token: "",
     userInfo: {},//用户信息
     isCollapse: false, //是否展开侧边栏
@@ -13,32 +12,31 @@ export default new Vuex.Store({
     uploadProgress: 0 //上传进度
   },
   mutations: {
-    addNum(state, payload = 1) {
-      state.num += payload
-    },
+    // 设置token
     setToken(state, payload = "") {
       state.token = "Bearer " + payload
     },
+    // 移除token
     removeToken(state) {
       state.token = ""
     },
+    // 设置用户信息
     setUserInfo(state, payload = {}) {
       state.userInfo = payload
     },
+    // 移除用户信息
     removeUserInfo(state) {
       state.userInfo = {}
     },
+    // 折叠菜单
     getCollapse(state, payload = false) {
       state.isCollapse = payload;
     },
+    // 上传进度
     setUploadProgress(state, payload = "") {
       state.uploadProgress = 0
       state.uploadProgress = payload;
     },
-  },
-  actions: {
-  },
-  modules: {
   },
   plugins: [createPersistedState()]
 })
