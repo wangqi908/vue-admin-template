feb57a4d922a76e05bf345cfe19deacffe803485
wangqi908 <wangqi908@hotmail.com>
2019-12-02 11:20:23 +0800

    用户更新优化图片存储问题

--- admin-server/routes/user/edit.js
+++ admin-server/routes/user/edit.js
@@ -1,16 +1,37 @@
 // 编辑
 const UserModel = require('../../db/models').UserModel
+const unlinkFile = require('../../utils/unlinkFile').unlinkFile
+const remove = require('../../utils/remove').remove //保存到永久文件夹
 
 const edit = (req, res) => {
   let { id } = req.body
-  UserModel.updateOne({ _id: id }, req.body, (err, doc) => {
+  // 先找到id对应的账户,拿到头像路径以便删除
+  UserModel.find({ _id: id }, async (err, doc) => {
     if (err) {
       res.send({ code: 0, data: err })
       return
     }
-    res.send({ code: 200, data: doc })
+    let userInfo = doc[0]
+    let newInfo = JSON.parse(JSON.stringify(req.body))
+    let avatar = userInfo.avatar
+    // 如果之前有图片,且需要更换图片 先删除之前的图片 然后更换图片新路径
+    if (avatar && newInfo.avatar) {
+      unlinkFile(avatar)
+    }
+    //头像新路径
+    if (newInfo.avatar) {
+      const newAvatarPath = await remove(newInfo.avatar)
+      newInfo.avatar = newAvatarPath
+    }
+    // 修改账户
+    UserModel.updateOne({ _id: id }, newInfo, (err, doc) => {
+      if (err) {
+        res.send({ code: 0, data: err })
+        return
+      }
+      res.send({ code: 200, data: doc })
+    })
   })
-
 };
 
 module.exports = {
--- admin-server/routes/user/remove.js
+++ admin-server/routes/user/remove.js
@@ -5,6 +5,7 @@ const unlinkFile = require('../../utils/unlinkFile').unlinkFile
 const remove = (req, res) => {
   let { ids } = req.body
   let idArr = { $in: ids }
+  // 先找到id对应的账户,拿到头像路径以便删除
   UserModel.find({ _id: idArr }, async (err, doc) => {
     if (err) {
       res.send({ code: 0, data: err })
