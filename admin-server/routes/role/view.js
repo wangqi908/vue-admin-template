3dd17368211baa99ea0dc9fd2f6f541af00e7632
wangqi908 <wangqi908@hotmail.com>
2019-12-11 16:42:42 +0800

    拆分getPermissionTreeByPermissionIds方法

--- admin-server/routes/role/view.js
+++ admin-server/routes/role/view.js
@@ -1,34 +1,7 @@
 // 详情
 const RoleSchema = require('../../db/models').RoleSchema
-const PermissionSchema = require('../../db/models').PermissionSchema
-const filterArray = require('../../utils').filterArray
+const getPermissionTreeByPermissionIds = require('../../utils').getPermissionTreeByPermissionIds// 通过权限ids拿到权限树
 const filter = { __v: 0 } // 查询时过滤出指定的属性
-
-// 通过权限ids拿到权限树
-const getPermissionTreeByPermissionIds = ids => {
-  return new Promise((resolve, reject) => {
-    PermissionSchema.find((err, permissionList) => {
-      if (err) {
-        reject(err)
-        return
-      }
-      let permissions = permissionList.filter(ele => ids.indexOf(ele._id) > -1)
-      let permissionIds = []
-      permissions.forEach(ele => {
-        if (ele.p_id != null && permissionIds.indexOf(ele.p_id)) {
-          permissionIds.push(ele.p_id)
-        }
-        if (permissionIds.indexOf(ele._id + "")) {
-          permissionIds.push(ele._id + "")
-        }
-      })
-      let allPermissions = permissionList.filter(ele => permissionIds.indexOf(ele._id + "") > -1)
-      let tree = filterArray(JSON.parse(JSON.stringify(allPermissions)))
-      resolve(tree)
-    }).sort({ index: 1 })
-  })
-
-}
 
 const view = async (req, res) => {
   let { _id } = req.body
--- admin-server/utils/index.js
+++ admin-server/utils/index.js
@@ -1,3 +1,6 @@
+
+const PermissionSchema = require('../db/models').PermissionSchema
+
 // 权限list转成tree
 const filterArray = (data, p_id) => {
   let tree = [];
@@ -15,5 +18,32 @@ const filterArray = (data, p_id) => {
   return tree;
 }
 
+// 通过权限ids拿到权限树
+const getPermissionTreeByPermissionIds = ids => {
+  return new Promise((resolve, reject) => {
+    PermissionSchema.find((err, permissionList) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      let permissions = permissionList.filter(ele => ids.indexOf(ele._id) > -1)
+      let permissionIds = []
+      permissions.forEach(ele => {
+        if (ele.p_id != null && permissionIds.indexOf(ele.p_id)) {
+          permissionIds.push(ele.p_id)
+        }
+        if (permissionIds.indexOf(ele._id + "")) {
+          permissionIds.push(ele._id + "")
+        }
+      })
+      let allPermissions = permissionList.filter(ele => permissionIds.indexOf(ele._id + "") > -1)
+      let tree = filterArray(JSON.parse(JSON.stringify(allPermissions)))
+      resolve(tree)
+    }).sort({ index: 1 })
+  })
+
+}
+
 exports.filterArray = filterArray
+exports.getPermissionTreeByPermissionIds = getPermissionTreeByPermissionIds
 
