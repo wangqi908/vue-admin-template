6fb13f07c138754978e1309f8374c24510bc009c
wangqi908 <wangqi908@hotmail.com>
2019-12-24 16:39:08 +0800

    滚动到屏幕顶端吸附自定义指令完成

--- admin-client/src/views/index/Index.vue
+++ admin-client/src/views/index/Index.vue
@@ -1,12 +1,34 @@
 <template>
   <div class='index'>
-    <h4>
-      首页
-    </h4>
+    <header>
+      sdfsdf
+    </header>
+    <div class="box" v-fixed></div>
+    <h1>
+      水电费水电费水电费
+    </h1>
   </div>
 </template>
 
 <script>
+import fixed from "@/directive/scroll";
 export default {
+  directives: {
+    fixed
+  }
 };
 </script>
+
+<style lang="scss">
+.index {
+  height: 2000px;
+  header {
+    padding: 80px;
+    border: 1px solid #ccc;
+  }
+  .box {
+    height: 60px;
+    background-color: rgb(179, 218, 255);
+  }
+}
+</style>
