97b0baebfecb6f1e418c9de8ebec3e6ebfa582bc
wangqi <wangqi908@hotmail.com>
2020-05-22 14:50:33 +0800

    配置打包地址判断

--- admin-client/src/apis/http.js
+++ admin-client/src/apis/http.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 import store from '@/store'
 import router from '@/router'
 import { showFullScreenLoading, tryHideFullScreenLoading, endLoading } from './loading'
-// export const baseURL = 'http://192.168.10.106/api/' //测试
-export const baseURL = 'http://localhost:8889/' //测试
+export const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8889/' : 'http://192.168.10.106/api/'
 
 export const timeout = 20000 // 设置超时时间
 
